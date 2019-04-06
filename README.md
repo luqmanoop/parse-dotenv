884f98acf2901ead9e2f4dcdce6731a2aa5d501d
Olushi Luqman <olushilukmon03@gmail.com>
2019-04-06 13:55:07 +0100

    build(travis): setup ci pipeline

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # parse-env
 
 > Zero dependency .env to object parser
+> [![Coverage Status](https://coveralls.io/repos/github/codeshifu/parse-env/badge.svg?branch=master)](https://coveralls.io/github/codeshifu/parse-env?branch=master)
--- test/lib.spec.js
+++ test/lib.spec.js
@@ -10,30 +10,34 @@ const DEFAULT_ENV_PATH = path.resolve(process.cwd(), '.env');
 const EXAMPLE_ENV_PATH = path.resolve(process.cwd(), '.env.example');
 
 describe('parseEnv', () => {
-  afterEach(() => sinon.restore());
+	afterEach(() => sinon.restore());
 
-  it('throws error for invalid env path', () => {
-    const errorMsg = 'Invalid env path';
-    try {
-      parseEnv('.env.sample');
-    } catch (error) {
-      expect(error.message).to.include('no such file');
-    }
-  });
+	it('throws error for invalid env path', () => {
+		const errorMsg = 'Invalid env path';
+		try {
+			parseEnv('.env.sample');
+		} catch (error) {
+			expect(error.message).to.include('no such file');
+		}
+	});
 
-  it('parse using default .env path', () => {
-    const spy = sinon.spy(fs, 'readFileSync');
-    parseEnv();
-    expect(spy).calledWith(DEFAULT_ENV_PATH);
-  });
+	it('parse using default .env path', () => {
+		const spy = sinon.spy(fs, 'readFileSync');
+		try {
+			parseEnv();
+		} catch (error) {
+		} finally {
+			expect(spy).calledWith(DEFAULT_ENV_PATH);
+		}
+	});
 
-  it('parse .env.example', () => {
-    const parsed = parseEnv('.env.example');
-    expect(parsed).to.have.deep.property('DB_CONNECTION', 'mysql');
-  });
+	it('parse .env.example', () => {
+		const parsed = parseEnv('.env.example');
+		expect(parsed).to.have.deep.property('DB_CONNECTION', 'mysql');
+	});
 
-  it('parse .env.example and include empty lnes', () => {
-    const parsed = parseEnv('.env.example', { emptyLines: true });
-    expect(parsed).to.have.deep.property('__EMPTYLINE_1__', '');
-  });
+	it('parse .env.example and include empty lnes', () => {
+		const parsed = parseEnv('.env.example', { emptyLines: true });
+		expect(parsed).to.have.deep.property('__EMPTYLINE_1__', '');
+	});
 });
