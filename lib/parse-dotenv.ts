b268aabcd2acdb5e1ce3c48556f32f305eb5ed2d
Olushi Luqman <olushilukmon03@gmail.com>
2019-04-07 14:28:19 +0100

    refactor(lint): configure eslint + prettier

--- lib/parse-dotenv.ts
+++ lib/parse-dotenv.ts
@@ -1,52 +1,55 @@
-import { resolve } from "path";
-import { readFileSync } from "fs";
+import { resolve } from 'path';
+import { readFileSync } from 'fs';
 
-const DEFAULT_ENV_PATH = resolve(process.cwd(), ".env");
-const NEWLINE = "\n";
+const DEFAULT_ENV_PATH = resolve(process.cwd(), '.env');
+const NEWLINE = '\n';
 
 interface Options {
-  emptyLines: boolean;
+	emptyLines: boolean;
 }
 
 interface EnvObject {
-  [key: string]: any;
+	[key: string]: any;
 }
 
 const parseEnv = (
-  path?: string,
-  opts: Options = { emptyLines: false }
+	path?: string,
+	opts: Options = { emptyLines: false }
 ): EnvObject => {
-  try {
-    const env = readFileSync(path || DEFAULT_ENV_PATH, {
-      encoding: "UTF-8"
-    });
-    const obj: EnvObject = {};
-    let newlineCount = 0;
-    env
-      .split(NEWLINE)
-      .map(
-        (line): string[] => {
-          if (line) {
-            return line.split("=").map(kv =>
-              kv
-                .trim()
-                .replace(/^("|')/, "")
-                .replace(/("|')$/, "")
-            );
-          } else if (opts.emptyLines) {
-            newlineCount++;
-            return [`__EMPTYLINE_${newlineCount}__`, ""];
-          } else return [""];
-        }
-      )
-      .filter(item => item.length > 1)
-      .forEach(item => {
-        obj[item[0]] = item[1];
-      });
-    return obj;
-  } catch (error) {
-    throw new Error(error);
-  }
+	try {
+		const env = readFileSync(path || DEFAULT_ENV_PATH, {
+			encoding: 'UTF-8',
+		});
+		const obj: EnvObject = {};
+		let newlineCount = 0;
+		env
+			.split(NEWLINE)
+			.map(
+				(line): string[] => {
+					if (line) {
+						return line.split('=').map(kv =>
+							kv
+								.trim()
+								.replace(/^("|')/, '')
+								.replace(/("|')$/, '')
+						);
+					}
+					if (opts.emptyLines) {
+						newlineCount += 1;
+						return [`__EMPTYLINE_${newlineCount}__`, ''];
+					}
+					return [''];
+				}
+			)
+			.filter(item => item.length > 1)
+			.forEach(item => {
+				const [key, value] = item;
+				obj[key] = value;
+			});
+		return obj;
+	} catch (error) {
+		throw new Error(error);
+	}
 };
 
 export default parseEnv;
--- test/parse-dotenv.spec.ts
+++ test/parse-dotenv.spec.ts
@@ -1,41 +1,42 @@
-import chai, { expect } from "chai";
-import fs from "fs";
-import path from "path";
-import sinon from "sinon";
-import sinonChai from "sinon-chai";
-import parseEnv from "../lib/parse-dotenv";
+import chai, { expect } from 'chai';
+import fs from 'fs';
+import path from 'path';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+import parseEnv from '../lib/parse-dotenv';
 
 chai.use(sinonChai);
-const DEFAULT_ENV_PATH = path.resolve(process.cwd(), ".env");
+const DEFAULT_ENV_PATH = path.resolve(process.cwd(), '.env');
 
-describe("parseEnv", () => {
-  afterEach(() => sinon.restore());
+describe('parseEnv', () => {
+	afterEach(() => sinon.restore());
 
-  it("throws error for invalid env path", () => {
-    try {
-      parseEnv(".env.sample");
-    } catch (error) {
-      expect(error.message).to.include("no such file");
-    }
-  });
+	it('throws error for invalid env path', () => {
+		try {
+			parseEnv('.env.sample');
+		} catch (error) {
+			expect(error.message).to.include('no such file');
+		}
+	});
 
-  it("parse using default .env path", () => {
-    const spy = sinon.spy(fs, "readFileSync");
-    try {
-      parseEnv();
-    } catch (error) {
-    } finally {
-      expect(spy).calledWith(DEFAULT_ENV_PATH);
-    }
-  });
+	it('parse using default .env path', () => {
+		const spy = sinon.spy(fs, 'readFileSync');
+		try {
+			parseEnv();
+		} catch (error) {
+			// eslint-disable-line
+		} finally {
+			expect(spy).calledWith(DEFAULT_ENV_PATH);
+		}
+	});
 
-  it("parse .env.example", () => {
-    const parsed = parseEnv(".env.example");
-    expect(parsed).to.have.deep.property("DB_CONNECTION", "mysql");
-  });
+	it('parse .env.example', () => {
+		const parsed = parseEnv('.env.example');
+		expect(parsed).to.have.deep.property('DB_CONNECTION', 'mysql');
+	});
 
-  it("parse .env.example and include empty lnes", () => {
-    const parsed = parseEnv(".env.example", { emptyLines: true });
-    expect(parsed).to.have.deep.property("__EMPTYLINE_1__", "");
-  });
+	it('parse .env.example and include empty lnes', () => {
+		const parsed = parseEnv('.env.example', { emptyLines: true });
+		expect(parsed).to.have.deep.property('__EMPTYLINE_1__', '');
+	});
 });
